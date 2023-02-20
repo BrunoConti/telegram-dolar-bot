867bc2d7393d66f4598ec1cdd9378b8268b6b70d
BrunoConti <bruno.conti1997@gmail.com>
2023-02-20 17:21:03 -0300

    Minor fix

--- src/bot.js
+++ src/bot.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 if(process.env.ENVIRONMENT === "Production") {
-  bot.startWebhook('/https://telegram-dolar-bot.vercel.app/', null, 8000);
+  bot.startWebhook('https://telegram-dolar-bot.vercel.app/', null, 8000);
 }
 
 bot.start(ctx => {
