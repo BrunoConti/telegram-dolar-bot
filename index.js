dfcc73b45fc4d54081fdabc3bac527b57a2e2a4e
BrunoConti <bruno.conti1997@gmail.com>
2023-02-20 18:30:18 -0300

    change port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if(process.env.ENVIRONMENT === 'Production') {
   bot.launch({
     webhook: {
         domain: process.env.DOMAIN,
-        port: process.env.PORT || 8000,
+        port: process.env.PORT || 443,
     }
   }).then(() => {
     console.info(`The bot ${bot.botInfo.username} is running on server`);
