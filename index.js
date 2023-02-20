c2e24b9409dfbbec5880e501c060a43d25983a5e
BrunoConti <bruno.conti1997@gmail.com>
2023-02-20 18:42:59 -0300

    Add dotenv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const bot = require('./src/bot');
 
+require('dotenv').config();
+
 if(process.env.ENVIRONMENT === 'Production') {
   bot.launch({
     webhook: {
         domain: process.env.DOMAIN,
-        port: process.env.PORT || 443,
+        port: process.env.PORT || 8000,
     }
   }).then(() => {
     console.info(`The bot ${bot.botInfo.username} is running on server`);
--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf');
 const { Scrapper, URL } = require('./scrapper');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
