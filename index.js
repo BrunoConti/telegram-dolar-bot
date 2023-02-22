b0972ba934c1bb2033e5150b32c09628b1b2bcb2
BrunoConti <bruno.conti1997@gmail.com>
2023-02-21 23:55:09 -0300

    test5

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if(process.env.ENVIRONMENT === 'Production') {
     res.send('💵 DOLAR BOT 💵')
   });
 
-  app.post(`/secret-path`, (req, res) => bot.handleUpdate(req.body, res));
+  app.post(`/secret-path`, async (req, res) => await bot.handleUpdate(req.body, res));
 
   app.listen(PORT, () => {
     console.log('listening on port', PORT);
